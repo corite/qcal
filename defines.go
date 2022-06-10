c0b9254d7cad91f3ae4852f5456421d0db861675
psic4t <psic4t@data.haus>
2022-06-10 13:46:01 +0200

    increment version in defines

--- defines.go
+++ defines.go
@@ -24,7 +24,7 @@ var endDateUTC string
 var summary string
 var toFile bool
 var elements []Event
-var qcalversion string = "0.8.6"
+var qcalversion string = "0.8.7"
 
 var colorBlock string = "|"
 var currentDot string = "•"
